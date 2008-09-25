d18aa2a9572029ce183c04e9df08413b7fb990ce
Rick Gibbed <gibbed@users.noreply.github.com>
2008-09-25 09:38:52 +0000

    - Added a creator value to the mod info that ModMaker adds to generated .packages. - Stub ModManager project that doesn't do anything (yet).

---
+++ Gibbed.Spore.ModManager/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gibbed.Spore.ModManager
+{
+	public partial class Manager : Form
+	{
+		public Manager()
+		{
+			InitializeComponent();
+		}
+	}
+}
